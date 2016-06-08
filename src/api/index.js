ce84e249baecd4f550769760f88786b0c3b8d25d
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-06-08 17:13:15 +0200

    Moved getMe to auth, might reconsider this.

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import { AuthResource, UserResource } from './resources'
 
 export default {
     localLogin: data => AuthResource.save({id: 'login'}, data),
-    getMe: () => UserResource.get({id:'me'}),
+    getMe: () => AuthResource.get({id:'me'}),
     mdUser: (data) => UserResource.update({id:'mdUser'},data),
     getUsers: () => UserResource.get({}),
 }
