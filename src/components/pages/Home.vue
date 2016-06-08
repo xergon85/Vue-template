89eafc8fc6b46affacf8878f9f474415fbb06f90
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-06-08 17:09:20 +0200

    Added Terms

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -10,13 +10,18 @@
                 <div class="app-tabs">
                     <ul class="nav app-tabs-stacked">
                         <li >
-                            <a v-link="{ path: '/home/welcome' }">
+                            <a v-link="{ name: 'home.welcome' }">
                                 <span><i class="fa fa-btn fa-fw fa-list"></i>&nbsp;{{ trans('general.welcome') }}</span>
                             </a>
                         </li>
                         <li >
-                            <a v-link="{ path: '/home/about' }">
+                            <a v-link="{ name: 'home.about' }">
                                 <span><i class="fa fa-btn fa-fw fa-lightbulb-o"></i>&nbsp;{{ trans('general.about') }}</span>
+                            </a>
+                        </li>
+                        <li >
+                            <a v-link="{ name: 'home.terms' }">
+                                <span><i class="fa fa-btn fa-fw fa-lightbulb-o"></i>&nbsp;{{ trans('general.terms') }}</span>
                             </a>
                         </li>
                     </ul>
