265ecebcffabe0049a2fadb74d4bfc6a820e0f1c
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-06-08 13:21:50 +0200

    Added .htaccess to support pushstate

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_ROOT = (process.env.NODE_ENV === 'production')
     ? 'http://api.yourdomain.dev'
-    : 'http://localhost:5000/api/'
+    : 'http://laravel-api.dev/api/'
 
 export const CookieDomain = (process.env.NODE_ENV === 'production')
     ? '.yourdomain.dev'
