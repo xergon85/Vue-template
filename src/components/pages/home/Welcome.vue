df9826caa3024e324f981d898806a893f7a40cd7
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2016-06-11 00:11:50 +0200

    Removed trash from the welcome page

--- src/components/pages/home/Welcome.vue
+++ src/components/pages/home/Welcome.vue
@@ -8,30 +8,23 @@
     </div>
     <div class="panel-body">
         {{ trans('general.welcome_content') }}
-        <button @click="showAlert">Alert</button>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
-    import { showMsg } from '../../../vuex/actions'
     import store from '../../../vuex/store'
 
     export default {
         data() {
             return {
-                alertMsg: 'testestest'
             }
         },
 
         vuex: {
             actions: {
-                showMsg
             }
         },
         methods: {
-            showAlert() {
-                showMsg(store, this.alertMsg)
-            }
         }
     }
 </script>
